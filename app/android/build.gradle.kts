0e47ce2333891ae8946861918c4c9d46bf55cdc1
Nightkilller <guptaa17aug@gmail.com>
2026-06-28 15:54:45 +0530

    fix(android): simplify subproject compileSdkVersion override to unconditionally target SDK 36

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -18,6 +18,14 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileSdkVersion(36)
+        }
+    }
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
